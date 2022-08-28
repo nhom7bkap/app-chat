58fd7f870e489b424c1d405fba662592403fbfe6
phongnh <phongnh@gmail.com>
2022-08-28 21:26:21 +0700

    update setting

--- app/src/main/java/com/team7/app_chat/ui/settings/SettingsFragment.java
+++ app/src/main/java/com/team7/app_chat/ui/settings/SettingsFragment.java
@@ -17,27 +17,36 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.bumptech.glide.Glide;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 import com.team7.app_chat.ChangePassActivity;
+import com.team7.app_chat.CurrentUser;
 import com.team7.app_chat.R;
 import com.team7.app_chat.SignInActivity;
 import com.team7.app_chat.UpdateProfileActivity;
+import com.team7.app_chat.models.User;
 
 import java.io.InputStream;
 import java.net.URL;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
+import de.hdodenhof.circleimageview.CircleImageView;
+
 public class SettingsFragment extends Fragment {
 
     private FirebaseAuth mAuth;
     private View view;
-    private TextView textViewEmail;
-    StorageReference storageReference;
-    FirebaseAuth firebaseProfile;
+    private User user;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        user = CurrentUser.user;
+    }
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -53,27 +62,23 @@ public class SettingsFragment extends Fragment {
                 startActivity(intent);
             }
         });
-
-        textViewEmail = view.findViewById(R.id.txtEmail);
-        firebaseProfile = FirebaseAuth.getInstance();
-        storageReference = FirebaseStorage.getInstance().getReference();
         showUserProfile();
 
-        StorageReference profileRef = storageReference.child("users").child(firebaseProfile.getCurrentUser().getUid()).child("profile.jpg");
-        profileRef.getDownloadUrl().addOnSuccessListener(uri -> {
-//                Picasso.get().load(uri).fit().centerCrop().into(profileImage);
-            Handler handler = new Handler(Looper.getMainLooper());
-            ExecutorService pool = Executors.newSingleThreadExecutor();
-            pool.execute(() -> {
-                try {
-                    InputStream url = new URL(uri.toString()).openStream();
-                    Bitmap bitmap = BitmapFactory.decodeStream(url);
-                    handler.post(() -> ((ImageView) view.findViewById(R.id.imageView4)).setImageBitmap(bitmap));
-                } catch (Exception e) {
-
-                }
-            });
-        });
+//        StorageReference profileRef = storageReference.child("users").child(firebaseProfile.getCurrentUser().getUid()).child("profile.jpg");
+//        profileRef.getDownloadUrl().addOnSuccessListener(uri -> {
+////                Picasso.get().load(uri).fit().centerCrop().into(profileImage);
+//            Handler handler = new Handler(Looper.getMainLooper());
+//            ExecutorService pool = Executors.newSingleThreadExecutor();
+//            pool.execute(() -> {
+//                try {
+//                    InputStream url = new URL(uri.toString()).openStream();
+//                    Bitmap bitmap = BitmapFactory.decodeStream(url);
+//                    handler.post(() -> ((ImageView) view.findViewById(R.id.imageView4)).setImageBitmap(bitmap));
+//                } catch (Exception e) {
+//
+//                }
+//            });
+//        });
         view.findViewById(R.id.textView4).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -92,13 +97,9 @@ public class SettingsFragment extends Fragment {
     }
 
     private void showUserProfile() {
-        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-        if (user == null) {
-            return;
-        } else {
-            String email = user.getEmail();
-            textViewEmail.setText(email);
-        }
+        ((TextView)view.findViewById(R.id.txtEmail)).setText(user.getEmail());
+        ImageView imageView = view.findViewById(R.id.profileAvatar);
+        Glide.with(view.getContext()).load(user.getAvatar()).into(imageView);
     }
 
 }
